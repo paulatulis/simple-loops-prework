759278ef6978ddbbe3438a9471590f5290ba50e9
Paula Tulis <tulis.paula@gmail.com>
2019-03-12 12:30:23 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,12 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  number_of_times = 0
+  7.times do 
+    puts "Welcome to Flatiron School's Web Development Course!"
+    number_of_times += 1 
+    end 
+  
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -13,12 +19,22 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
+   number_of_times = 0
+  7.times do 
+    puts "Welcome to Flatiron School's Web Development Course!"
+    number_of_times += 1 
+    end 
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
 
 def while_iterator(number_of_times)
+  number_of_times = 0 
+  while number_of_times < 7 
+  puts "Welcome to Flatiron School's Web Development Course!"
+  number_of_times += 1 
+end 
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
@@ -26,6 +42,12 @@ def while_iterator(number_of_times)
 end
 
 def until_iterator(number_of_times)
+  number_of_times = 0 
+  until number_of_times == 7
+  puts "Welcome to Flatiron School's Web Development Course!"
+  number_of_times += 1 
+end 
+
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
@@ -33,6 +55,10 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
+  number_of_times = 1..7
+  for number in number_of_times
+  puts "Welcome to Flatiron School's Web Development Course!"
+end 
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
