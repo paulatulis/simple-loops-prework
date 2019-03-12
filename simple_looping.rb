433923e5bd7ea9d7bcb01cd069bae2d926a54629
Paula Tulis <tulis.paula@gmail.com>
2019-03-12 12:25:49 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -25,6 +25,11 @@ def times_iterator(number_of_times)
 end
 
 def while_iterator(number_of_times)
+  number_of_times = 0 
+  while number_of_times < 7 
+  puts "Welcome to Flatiron School's Web Development Course!"
+  number_of_times += 1 
+end 
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
